20f77b2a0b04d827fbce61fdab1725b124f76f63
mik <mail@kissel.ch>
2024-06-04 22:19:38 +0200

    work

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sqlite3
+
+
+
+
+print("New db entry")
 con = sqlite3.connect("data/blog.db")
 
 cur = con.cursor()
@@ -9,6 +14,6 @@ cur.execute("""
     insert into picblog values
         ('2024.04.18', 'Chateaux Meyney', 'best', 'IMG_4058.jpg')
 """)
-
+print("Entry done")
 
 con.commit()
